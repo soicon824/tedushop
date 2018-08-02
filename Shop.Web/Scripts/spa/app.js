89f2b31e748715f3bfbf8b44104c24fc26ed6faf
soicon824 <phamthienphuc@gmail.com>
2018-08-02 23:16:56 +0700

    xong bai 20

--- Shop.Web/Scripts/spa/app.js
+++ Shop.Web/Scripts/spa/app.js
@@ -1,13 +1,20 @@
 ﻿var myApp = angular.module('myModule', []);
 myApp.controller("schoolController", schoolController);
-myApp.service('validator', validator)
+myApp.service('validator', validator);
+
+myApp.directive("teduShopDirective", function () {
+    return {
+        restrict: 'E',
+        templateUrl: "/Scripts/spa/tedushopdirective.html"
+    }
+});
+
 function schoolController($scope, validator) {
     $scope.checkNumber = function (num) {
         $scope.message = validator.checkNumberMain(num);
 
     }
     $scope.num = 1;
-    
 }
 
 function validator($window) {
