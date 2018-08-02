860094b118df746d56b5eae28495dd38742cea6e
soicon824 <phamthienphuc@gmail.com>
2018-08-02 22:45:39 +0700

    hoan thanh bai 18

--- Shop.Web/Scripts/spa/app.js
+++ Shop.Web/Scripts/spa/app.js
@@ -1,6 +1,17 @@
 ﻿var myApp = angular.module('myModule', []);
-myApp.controller("myController", myController);
+myApp.controller("schoolController", schoolController);
+myApp.controller("studentController", studentController);
+myApp.controller("teacherController", teacherController);
+
 myApp.$inject = ['$scope'];
-function myController($scope) {
+function schoolController($scope) {
     $scope.message = "This is my message from controller";
+}
+
+function studentController($scope) {
+    $scope.message = "This is my studentController";
+}
+
+function teacherController($scope) {
+    //$scope.message = "This is my teacherController";
 }
