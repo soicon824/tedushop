17d0e9dc38e1137db0269fe39c4a8f7235e694af
soicon824 <phamthienphuc@gmail.com>
2018-08-02 22:28:21 +0700

    hoan thanh bai 17

---
+++ Shop.Web/Scripts/spa/app.js
@@ -0,0 +1,6 @@
+﻿var myApp = angular.module('myModule', []);
+myApp.controller("myController", myController);
+myApp.$inject = ['$scope'];
+function myController($scope) {
+    $scope.message = "This is my message from controller";
+}
